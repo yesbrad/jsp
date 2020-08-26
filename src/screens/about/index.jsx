a61bfc2064ab2609accb8f32e9a2030f040c8c8f
brad <b.bradfrancis@gmail.com>
2020-08-26 19:22:36 +1000

    Added Firebase

---
+++ src/screens/about/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.scss';
+import Navigation from '../../components/navigation';
+import Footer from '../../components/footer';
+
+const About = () => {
+	return (  
+		<div className="about-container">
+			<Navigation />
+				About
+			<Footer />
+		</div>
+	);
+}
+ 
+export default Service;
---
+++ src/screens/contact/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.scss';
+import Navigation from '../../components/navigation';
+import Footer from '../../components/footer';
+
+const Contact = () => {
+	return (  
+		<div className="contact-container">
+			<Navigation />
+				Contact
+			<Footer />
+		</div>
+	);
+}
+ 
+export default Service;
