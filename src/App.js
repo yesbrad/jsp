9e89bf9893bc1e76889ed78d0fd8cbe18c0e053a
brad <b.bradfrancis@gmail.com>
2020-08-28 15:32:13 +1000

    Added Page Transitions

--- src/App.js
+++ src/App.js
@@ -4,40 +4,53 @@ import {
 	Switch,
 	Route,
 	useParams,
-	Link
+	Link,
+	useLocation
 } from "react-router-dom";
 import Home from './screens/home';
 import Service from './screens/service';
 import Contact from './screens/contact';
 import About from './screens/about';
-  
+import { useTransition, animated } from 'react-spring'
+
 const RenderService = () => {
 	let { id } = useParams();
 
 	return (
-		<div>
+		<div style={{position: 'absolute', width: '100%', height: '100%'}}>
 			<Service service={id}/>
 		</div>
 	)
 }
 
-const App = () => {
+const AppTrans = () => {
+  const location = useLocation()
 
-  return (
-	  <div className="App">
-		  <Router>
-				<Switch>
-					<Route path='/service/:id'>
-						<RenderService />
-					</Route>
-					<Route path='/services'><Home goToService /></Route>
-					<Route path='/about-us'><About /></Route>
-					<Route path='/contact'><Contact /></Route>
-					<Route path='/'><Home /></Route>
+  const transitions = useTransition(location, location => location.pathname, {
+    from: { opacity: 0},
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  })
+
+  return transitions.map(({ item: location, props, key }) => (
+		<animated.div key={key} style={props}>
+			<Switch location={location}>
+				<Route path='/service/:id'>
+					<RenderService />
+				</Route>
+				<Route path='/services'><div style={{position: 'absolute', width: '100%', height: '100%'}}><Home goToService /></div></Route>
+				<Route path='/about-us'><div style={{position: 'absolute', width: '100%', height: '100%'}}><About /></div></Route>
+				<Route path='/contact'><div style={{position: 'absolute', width: '100%', height: '100%'}}><Contact /></div></Route>
+				<Route path='/'><div style={{position: 'absolute', width: '100%', height: '100%'}}><Home /></div></Route>
 				</Switch>
-		  </Router>
-    </div>
-  );
+		</animated.div>
+  ));
 }
+
+const App = () => (
+	<Router>
+		<AppTrans />
+	</Router>
+)
 
 export default App;
