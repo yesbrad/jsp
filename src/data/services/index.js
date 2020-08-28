7057864c8484e81e406e6523fd26ab43822ebd64
brad <b.bradfrancis@gmail.com>
2020-08-28 15:16:52 +1000

    Content Pass

--- src/data/services/index.js
+++ src/data/services/index.js
@@ -1,11 +1,11 @@
 const data = [
 	{
-		name: 'Burst Pipe',
-		url: 'burst-pipe',
-		miniDescription: 'Burst Pipes Mini Description',
+		name: 'General Plumbing Maintenance',
+		url: 'general-plumbing-maintenance',
+		miniDescription: 'Everything General Plumbing',
 		content: `
-		Using modern specialised tooling, we can repair most types of leaking water pipes such as Copper, Pex pipe, Rehau, Galvanised Iron and other types. If the leak cannot be visibly seen, we can use our state of the art pipe locating and acoustic leak detection equipment to precisely locate the area of leakage with minimum disturbance of your assets.		`,
-		imageURI: require('../../images/VanDouble1.jpg')
+		We can clear all types of blocked drains, from basins, showers, toilets, kitchen sinks, main sewer and storm water drains using modern machinery and/or hand tools. Most drains are cleared within an hour, with a minimum of fuss and a no mess approach.		`,
+		imageURI: require('../../images/General.jpg')
 	},
 	{
 		name: 'Blocked Drains',
@@ -13,7 +13,7 @@ const data = [
 		miniDescription: 'Burst Pipes Mini Description',
 		content: `
 		We can clear all types of blocked drains, from basins, showers, toilets, kitchen sinks, main sewer and storm water drains using modern machinery and/or hand tools. Most drains are cleared within an hour, with a minimum of fuss and a no mess approach.		`,
-		imageURI: require('../../images/VanDouble1.jpg')
+		imageURI: require('../../images/Blocked.jpg')
 	},
 	{
 		name: 'Hot Water Services',
@@ -22,8 +22,9 @@ const data = [
 		content: `
 		We maintain all brands of gas and electric hot water services, both mains pressure units, and ‘in roof’ storage tanks. Leaking pressure and temperature relief valves, ball valves, pilot light failure and leaking hot water units are all a part of our service. If we cannot repair your hot water unit we will assess your hot water requirements and offer you the best replacement unit at a budget that suits you.
 		`,
-		imageURI: require('../../images/VanDouble1.jpg')
+		imageURI: require('../../images/HotWater.jpg')
 	},
+	
 	{
 		name: 'Tap & Toilet Repairs',
 		url: 'tap-toilet-repairs',
@@ -31,8 +32,52 @@ const data = [
 		content: `
 		For conventional taps, we will fully recondition your ‘hard to turn off’ taps using the best quality parts so you can effortlessly turn off your dripping taps. For ‘Flick mixer type’ taps, that are leaking, we can replace the cartridge ‘at call’ as we stock most common types on board our service vehicle. As with any type of tapware, if its not worth repairing, we can also replace them at minimal cost.
 		`,
-		imageURI: require('../../images/VanDouble1.jpg')
+		imageURI: require('../../images/Tap.jpg')
 	},
+	{
+		name: 'Burst Pipe',
+		url: 'burst-pipe',
+		miniDescription: 'Burst Pipes Mini Description',
+		content: `
+		Using modern specialised tooling, we can repair most types of leaking water pipes such as Copper, Pex pipe, Rehau, Galvanised Iron and other types. If the leak cannot be visibly seen, we can use our state of the art pipe locating and acoustic leak detection equipment to precisely locate the area of leakage with minimum disturbance of your assets.		`,
+		imageURI: require('../../images/Burst.jpg')
+	},
+	{
+		name: 'Roof Repairs & Guttering Replacement',
+		url: 'roof-repairs',
+		miniDescription: '',
+		content: `We carry out all repairs on steel and colorbond roofing, from locating small leaks to replacement . Replacement of fascia guttering, down pipes and box gutters, both colorbond and zincalume steel are part of our services.`,
+		imageURI: require('../../images/RoofRepairs.jpg')
+	},
+	{
+		name: 'Leak Detection/Pipe & Drain Locating',
+		url: 'leak-detection-drain-locating',
+		miniDescription: '',
+		content: `All types of ‘unseen water leakage’ can be detected using our electronic acoustic leak detection equipment, possibly saving you many hours of digging and destructive work. Or if you need to know the location of your drains or water pipes prior to landscaping or building works, we can also locate these for you.`,
+		imageURI: require('../../images/Leak.jpg')
+	},
+	{
+		name: 'Gas Appliance Servicing',
+		url: 'gas-appliance-servicing',
+		miniDescription: '',
+		content: `Government Authorities recommend that all gas appliances should be serviced by a licenced gas fitter every 2 years. The reason for this is the safety of you and your family. We can service your space heater, wall furnace or central heater to assure you that your heater is both safe and economical to use. We will inspect your heater for carbon monoxide spillage, flue condition, burner gas pressure, integrity of the installation and thoroughly clean the heater .`,
+		imageURI: require('../../images/Gas.jpg')
+	},
+	{
+		name: 'TMV Testing & Servicing',
+		url: 'tmv-testing-servicing',
+		miniDescription: '',
+		content: `For schools, aged care, kindergartens and healthcare facilities, we are licenced to test and maintain Thermostatic Mixing Valves (TMV’s) to satisfy your legal obligations. TMV’s should be tested as per government regulations, every 2 years and should be replaced or reconditioned every 5 years`,
+		imageURI: require('../../images/TZMC.jpg')
+	},
+	{
+		name: 'Water Tanks',
+		url: 'water-tanks',
+		miniDescription: '',
+		content: `We can install a variety of different style water tanks complete with pump, so you can water your garden or wash cars without the cost or waste of mains supplied water. We can also connect the tank water to your toilets and washing machine, saving you even more money on your water bill.`,
+		imageURI: require('../../images/Water.jpg')
+	},
+	
 ]
 
 export default data;
